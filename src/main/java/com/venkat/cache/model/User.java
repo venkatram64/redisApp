fe83eefff86b81d05f4170f6dc52b417fa5ad666
venkatram64 <venkat.veerareddy@hotmail.com>
2020-04-28 11:33:40 +0530

    added test case

--- src/main/java/com/venkat/cache/model/User.java
+++ src/main/java/com/venkat/cache/model/User.java
@@ -39,4 +39,13 @@ public class User implements Serializable {
     public void setSalary(Long salary) {
         this.salary = salary;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
---
+++ src/test/java/com/venkat/cache/repository/UserRepositoryImplTest.java
@@ -0,0 +1,52 @@
+package com.venkat.cache.repository;
+
+
+import com.venkat.cache.model.User;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import redis.embedded.RedisServerBuilder;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryImplTest {
+
+    private static redis.embedded.RedisServer redisServer;
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @BeforeClass
+    public static void startRedisServer() throws IOException {
+        redisServer = new RedisServerBuilder().port(6379).setting("maxmemory 256M").build();
+        redisServer.start();
+    }
+
+    @AfterClass
+    public static void stopRedisServer() throws IOException {
+        redisServer.stop();
+    }
+
+    @Test
+    public void saveTest(){
+        User user = new User();
+        user.setId("1");
+        user.setName("Venkatram");
+        user.setSalary(123456789L);
+        userRepository.save(user);
+        User newUser = userRepository.findById("1");
+        System.out.println("New Uer is " + newUser);
+        assertEquals(user.getName(), newUser.getName());
+    }
+}
